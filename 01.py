5a6235d3e2a8d0001a811714e672988f75d652df
mayke <mayke.salles.soares@gmail.com>
2023-07-19 09:45:53 -0300

    Criando Tabuada

---
+++ 01.py
@@ -0,0 +1,19 @@
+#CURSO EM VÍDEO PYTHON
+
+#Faça um progrma que leia um número inteiro qualquer e mostre ne tela a sua tabuada.
+num = int(input("Digte um número para ver sua tabuada: "))
+#Ilustração de linha tracejada usando "-" * 12 para mostrar na tela.
+print("-"*12)
+#Print usando operadores e fazendo operações dentro do format.
+print("{} x {:2} = {}".format(num, 1, num*1))
+print("{} x {:2} = {}".format(num, 2, num*2))
+print("{} x {:2} = {}".format(num, 3, num*3))
+print("{} x {:2} = {}".format(num, 4, num*4))
+print("{} x {:2} = {}".format(num, 5, num*5))
+print("{} x {:2} = {}".format(num, 6, num*6))
+print("{} x {:2} = {}".format(num, 7, num*7))
+print("{} x {:2} = {}".format(num, 8, num*8))
+print("{} x {:2} = {}".format(num, 9, num*9))
+print("{} x {:2} = {}".format(num, 10, num*10))
+#finalisando com print ilustrativo de linha tracejada.
+print("-"*12)
