a4479d0b38335f9475953f848aa17803e54d9e57
mayke <mayke.salles.soares@gmail.com>
2023-07-20 13:04:45 -0300

    Exercício04

---
+++ 04.py
@@ -0,0 +1,6 @@
+#Para dentro de (c) na distância de:
+for c in range(0, 5):
+#O fator de está endentado em (for) a variável (n) é recebida.
+    n = int(input("Digite um número: "))
+#Fim não endentado em for não é obrigatório.
+print("Fim.")
