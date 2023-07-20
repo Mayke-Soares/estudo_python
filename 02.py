b74997f65664541b23b003a001fefa43d8287946
mayke <mayke.salles.soares@gmail.com>
2023-07-20 11:12:16 -0300

    aula 2

--- 02.py
+++ 02.py
@@ -1,2 +1,7 @@
-lista = [/,/,/,/,/,/,/]
-print(lista)
+#LAÇO DE REPETIÇÃO
+
+num = int(input("Digite um número"))
+#Para dentro de C na distancia de: 
+for             c in   range      (0, num):
+#Escrever na tela a variável c usada em for
+    print(c)
