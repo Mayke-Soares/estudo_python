2242739f6a8b11a4577baf097ec74f93a5cef94e
mayke <mayke.salles.soares@gmail.com>
2023-07-23 20:16:51 -0300

    for

--- 05.py
+++ 05.py
@@ -3,5 +3,6 @@ s = 0
 for c in range(0, 5):
 #O fator de está endentado em (for) a variável (n) é recebida.
     n = int(input("Digite um número: "))
+#Somando (s) dos 5 resultados
     s += n
 print("{}".format(s))
