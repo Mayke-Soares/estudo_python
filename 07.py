eb236336868eaed3ec882101d5b4a5aeabbfd5fb
mayke <mayke.salles.soares@gmail.com>
2023-07-23 20:36:03 -0300

    while e input

---
+++ 07.py
@@ -0,0 +1,7 @@
+#Variável (n) recebe (1)
+n = 1 
+#Enquanto (n) for diferente de (0)
+while n != 0:
+#Os números digitados serão lidos até que se digite (0)
+    n = int(input("Digite um valor: "))
+print("Fim")
