10f23610411acd56c1c8e372b6f2fc866b9d3b06
mayke <mayke.salles.soares@gmail.com>
2023-07-23 20:07:16 -0300

    relação for e while

--- 05.py
+++ 05.py
@@ -1,5 +1,7 @@
 s = 0
+#Para dentro de (c) na distância de:
 for c in range(0, 5):
+#O fator de está endentado em (for) a variável (n) é recebida.
     n = int(input("Digite um número: "))
     s += n
 print("{}".format(s))
