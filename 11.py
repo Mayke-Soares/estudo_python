e180fd5fd6faa34e2257d252efbbd2eadd6d358f
mayke <mayke.salles.soares@gmail.com>
2023-07-25 10:15:37 -0300

    lower e upper

---
+++ 11.py
@@ -0,0 +1,4 @@
+nome = str(input("Digite seu nome: "))
+print("Analizando seu nome...")
+print("Seu nome em maiúsculas é {}".format(nome.upper()))
+print("Seu nome em minúsculas é {}".format(nome.lower()))
