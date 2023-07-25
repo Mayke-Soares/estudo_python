db9b652c05d0fa096a519eb2343626db40c003c9
mayke <mayke.salles.soares@gmail.com>
2023-07-25 10:52:30 -0300

    upper,lower,count,find

--- 11.py
+++ 11.py
@@ -1,4 +1,6 @@
-nome = str(input("Digite seu nome: "))
+nome = str(input("Digite seu nome: ")).strip()
 print("Analizando seu nome...")
 print("Seu nome em maiúsculas é {}".format(nome.upper()))
 print("Seu nome em minúsculas é {}".format(nome.lower()))
+print("Seu nome tem {} letras".format(len(nome)- nome.count(" ")))
+print("Seu primeiro nome tem {} letras".format(nome.find(" ")))
