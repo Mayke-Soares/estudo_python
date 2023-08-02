f4359772b88537318bc72739cb655d1bb1b13132
mayke <mayke.salles.soares@gmail.com>
2023-08-02 10:33:39 -0300

    if e else, condição simplificada

---
+++ 19.py
@@ -0,0 +1,6 @@
+nome = str(input("Digite seu nome: "))
+if nome == "Gustavo":
+    print("Você é um cara muito gente boa!")
+else:
+    print("Você não é a pessoa certa.")
+print("Bom dia, {}!".format(nome))
